3e5e45a560628a5b55e688f5c1ce21a3510e8c6a
Denys Ulyanov <ulyanovdenis@gmail.com>
2024-11-01 12:25:20 +0100

    Add tests for weatherstack, update config with base url and key

---
+++ tests/api_tests/test_weatherstack.py
@@ -0,0 +1,77 @@
+import os
+
+import pytest
+import requests
+import yaml
+
+
+# Fixture to load configuration from config.yaml
+@pytest.fixture(scope="session")
+def config():
+    config_path = os.path.join(os.path.dirname(__file__), '../../configs/config.yaml')
+    with open(config_path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+@pytest.fixture
+def base_url(config):
+    return config['weatherstack']['url']
+
+
+@pytest.fixture
+def valid_api_key(config):
+    return config['weatherstack']['key']
+
+
+# Test Case 1: Valid API Response Test
+@pytest.mark.parametrize("location", [
+    ("Amsterdam"),
+    ("New York"),
+    ("London")
+])
+def test_valid_response(location, valid_api_key, base_url):
+    params = {
+        "access_key": valid_api_key,
+        "query": location
+    }
+
+    response = get_weather(base_url, params)
+
+    # Validate response status
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+
+    # Verify provided data
+    data = response.json()
+
+    # Verify location is matching requested one
+    assert data["location"]["name"] == location
+
+    # Verify current weather forecast is not missing or empty
+    current_weather = data["current"]
+    assert current_weather is not None, "'current' weather data is missing"
+    assert len(current_weather) > 0, "'current' weather data is empty"
+
+
+# Test Case 2: Invalid API Key Test
+def test_invalid_api_key(base_url):
+    params = {
+        "access_key": "this_is_invalid_key",
+        "query": "New York"
+    }
+
+    response = get_weather(base_url, params)
+
+    # Validate that an error is returned
+    assert response.status_code == 200, f"Unexpected status code: {response.status_code}"
+
+    # Verify error codes
+    data = response.json()
+
+    assert data["success"] == False
+    assert data["error"]["code"] == 101
+    assert data["error"]["type"] == "invalid_access_key"
+    assert "You have not supplied a valid API Access Key" in data["error"]["info"]
+
+
+def get_weather(base_url, params):
+    return requests.get(base_url + "/current", params=params)
