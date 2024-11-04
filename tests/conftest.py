f9138886ba63e77b4ff5ea80a35940f338ed6430
Den <3912300+DenUlyanov@users.noreply.github.com>
2024-11-04 14:47:41 +0100

    Run test in Github Actions  (#13)
    Changes
    Update .github/workflows/main.yml to run on any branch
    Start using a headless browser
    Add requirements.txt

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,9 @@ from selenium.webdriver.chrome.options import Options
 def init_driver(request):
     chrome_options = Options()
     chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--start-maximized")
